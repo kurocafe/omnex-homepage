49df6ae942636f167a761742590406b7d30fae4c
kurocafe <aiue7298@gmail.com>
2025-04-25 13:41:24 +0900

    Add About and Home pages, implement Footer component, and update global styles

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="absolute bottom-0 w-full py-2 bg-gray-200 text-center text-sm text-black">
+      onmex co., ltd
+    </footer>
+  )
+}
