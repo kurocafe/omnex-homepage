ea5bb83aea2b0b3dce275bc07630dd6fccdaf31c
kurocafe <aiue7298@gmail.com>
2025-05-17 04:14:54 +0900

    Logoコンポーネントを新規作成し、ロゴとキャッチフレーズを表示する機能を追加

---
+++ src/components/logo.tsx
@@ -0,0 +1,13 @@
+export default function Logo() {
+  return (
+    <header className="pt-16 ol-24 text-center">
+      <h1 className="font-serif text-5xl font-bold mb-2">
+        omnex
+      </h1>
+      <p className="font-serif text-lg">
+        Beyond Borders,<br />
+        Beyond Limits
+      </p>
+    </header>
+  )
+}
