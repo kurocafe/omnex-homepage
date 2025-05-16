4a8518dee407430014caaaeb9996b65cc039b367
kurocafe <aiue7298@gmail.com>
2025-05-17 04:46:59 +0900

    Homeコンポーネントのレイアウトを修正し、InfoButtonコンポーネントを使用してナビゲーション機能を追加。InfoButtonのスタイルを改善し、画像サイズを調整。

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,44 @@
+import InfoButton from "@/components/infoButton";
+import Logo from "@/components/logo";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">Welcome to My App</h1>
-        <p className="text-lg">This is a simple Next.js application.</p>
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <div className="absolute mx-15">
+        <Logo />
+      </div>
+
+      <main className="flex-1 flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center space-y-8">
+          <div className="flex space-x-60">
+            <Link href={"/about"}>
+              <InfoButton
+                title="ABOUT US"
+                imageUrl="/IMG_0642.png"
+                alt="about us"
+              />
+            </Link>
+            <Link href={"/"}>
+              <InfoButton
+                title="THE PROJECTS"
+                imageUrl="/IMG_0643.png"
+                alt="the projects"
+              />
+            </Link>
+          </div>
+          <div className="">
+            <Link href={"/"}>
+              <InfoButton
+                title="COLAB WITH US"
+                imageUrl="/IMG_0644.png"
+                alt="about us"
+              />
+            </Link>
+          </div>
+        </div>
       </main>
+
     </div>
   );
 }
--- src/components/infoButton.tsx
+++ src/components/infoButton.tsx
@@ -3,23 +3,24 @@ import Image from "next/image";
 
 type InfoButtonProps = {
   title: string;
+  pageUrl?: string;
   imageUrl: string;
   alt?: string;
 };
 
 export default function InfoButton({ title, imageUrl, alt }: InfoButtonProps) {
   return (
-    <div className="bg-white bg-opacity-60 border-4 border-white rounded-2xl p-4 w-72 flex flex-col items-center shadow-md">
+    <button className="bg-cyan-30 bg-opacity-60 border-4 border-white rounded-2xl p-4 w-72 flex flex-col items-center shadow-md">
       <span className="font-serif font-semibold text-lg mb-2 self-start">
         {title}
       </span>
       <Image
         src={imageUrl}
         alt={alt || title}
-        width={240}
-        height={128}
-        className="relative w-60 h-32 object-cover rounded-[40px]"
+        width={480}
+        height={256}
+        className="relative w-60 h-32 object-cover rounded-[70px]"
       />
-    </div>
+    </button>
   )
 }
