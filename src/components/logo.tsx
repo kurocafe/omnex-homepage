af4d8502632c873a2ecd907ec934e1d96e1bf939
kurocafe <aiue7298@gmail.com>
2025-05-29 22:54:01 +0900

    LogoコンポーネントをLinkでラップし、クリック可能なヘッダーに変更

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,18 @@
+import Link from "next/link";
+
 export default function Logo() {
   return (
-    <header className="pt-8 ol-24 text-center">
-      <h1 className="font-serif text-5xl font-bold mb-2">
-        omnex
-      </h1>
-      <p className="font-serif text-lg">
-        Beyond Borders,<br />
-        Beyond Limits
-      </p>
-    </header>
+    <Link href={"/"}>
+      <header className="pt-8 ol-24 text-center">
+        <h1 className="font-serif text-5xl font-bold mb-2">
+          omnex
+        </h1>
+        <p className="font-serif text-lg">
+          Beyond Borders,<br />
+          Beyond Limits
+        </p>
+      </header>
+    </Link>
+
   )
 }
