bb081053be17160af30d595142d167a5f09179a5
kurocafe <aiue7298@gmail.com>
2025-07-18 18:54:03 +0900

    feat: 更新された背景画像とオーバーレイの透明度を改善

--- src/app/marketplace/products/page.tsx
+++ src/app/marketplace/products/page.tsx
@@ -6,9 +6,9 @@ export default function Products() {
     <div className="relative w-full min-h-screen">
       <div
         className="pointer-events-none fixed inset-0 bg-cover bg-center bg-no-repeat z-0"
-        style={{ backgroundImage: "url(/IMG_0649.jpg)" }}
+        style={{ backgroundImage: "url(/market.png)" }}
       />
-      <div className="absolute inset-0 bg-black/25" />
+      <div className="absolute inset-0 bg-black/40" />
 
       <div className="relative z-10 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] text-white">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start font-serif">
