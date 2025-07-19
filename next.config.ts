214ef3a4306e1a674474b829fc05ad74f71d7571
kurocafe <aiue7298@gmail.com>
2025-07-20 00:34:44 +0900

    feat: next.config.tsからbasePathとassetPrefixを削除し、package.jsonのbuildスクリプトを修正

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/omnex-homepage',
-  assetPrefix: '/omnex-homepage/',
 };
 
 export default nextConfig;
